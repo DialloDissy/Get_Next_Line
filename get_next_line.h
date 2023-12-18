f77e8635de96b3fa1694b1bcb288a45642f4eb8c
diallo sidy <diallodissy@gmail.com>
2023-12-18 17:28:01 +0100

    correct

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sidiallo <sidiallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 10:45:00 by sidiallo          #+#    #+#             */
-/*   Updated: 2023/12/18 15:40:23 by sidiallo         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:27:00 by sidiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@
 char	*ft_strchr(char *str, int c);
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(char *s);
-char	*ft_strdup(const char *s);
 char	*get_next_line(int fd);
 char	*get_line(char *str);
 char	*read_line(int fd, char *save);
